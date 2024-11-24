887952cbbb3558b37ac4f7b6fdee509299b422c1
BushljaJaroslav <nefortyuiu24@outlook.com>
2024-11-24 21:44:35 +0500

    Task0

--- Tyuiu.BushlyaYaV.Sprint5.Task0.V20.Lib/DataService.cs
+++ Tyuiu.BushlyaYaV.Sprint5.Task0.V20.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.BushlyaYaV.Sprint5.Task0.V20.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
-            double x1 = Math.Round(2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + 4.1 * x, 3);
+            double x1 = Math.Round(2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + 4.1 * x * 2, 3);
             File.WriteAllText(path, x1.ToString());
             return path;
             
--- Tyuiu.BushlyaYaV.Sprint5.Task0.V20.Test/DataServiceTest.cs
+++ Tyuiu.BushlyaYaV.Sprint5.Task0.V20.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.BushlyaYaV.Sprint5.Task0.V20.Test
         {
             DataService ds = new DataService();
             var res = ds.SaveToFileTextData(2);
-            Assert.AreEqual(File.ReadAllText(res), (29.36).ToString());
+            Assert.AreEqual(File.ReadAllText(res), (37.56).ToString());
         }
     }
 }
