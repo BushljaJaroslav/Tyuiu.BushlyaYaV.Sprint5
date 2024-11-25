681dfe10a2bf8eed94ec356b8039185497268adb
BushljaJaroslav <nefortyuiu24@outlook.com>
2024-11-25 23:57:08 +0500

    Task2

---
+++ Tyuiu.BushlyaYaV.Sprint5.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿
+using Tyuiu.BushlyaYaV.Sprint5.Task2.V26.Lib;
+
+namespace Tyuiu.BushlyaYaV.Sprint5.Task2.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(fileExists, wait);
+        }
+    }
+}
