3eec0537be6a06d78f54b96c186d42c8d2e3fbae
BushljaJaroslav <nefortyuiu24@outlook.com>
2024-11-26 23:44:09 +0500

    Task5

---
+++ Tyuiu.BushlyaYaV.Sprint5.Task5.V18.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.BushlyaYaV.Sprint5.Task5.V18.Lib
+{
+    public class DataService : ISprint5Task5V18
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 1;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string strX = File.ReadAllText(path);
+                strX = strX.Replace(".", ",");
+                
+                double x;
+                while ((strX = reader.ReadLine()) != null)
+                {
+                    x = Convert.ToDouble(strX);
+                    if (Math.Abs(x) >= 10 && Math.Abs(x) <= 99)
+                    {
+                        res *= x;
+                    }
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
